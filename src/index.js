cc2f8c0fda9f20d468cb2ad4d446c04f0ef6e05e
tonare <carlos.renato.simoes@gmail.com>
2019-03-14 17:24:07 -0300

    new css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './css/index.css'
-import './css/404.css'
 import App from './components/App'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
