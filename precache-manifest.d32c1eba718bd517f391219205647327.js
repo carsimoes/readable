af12149dad6466dcb25a067b68d6fc4405eb41c5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-16 15:35:57 +0000

    Deploy carsimoes/readable to github.com/carsimoes/readable.git:gh-pages

---
+++ precache-manifest.d32c1eba718bd517f391219205647327.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bd9203c75e4bcf9d21c",
+    "url": "/static/css/main.b3bc6d04.chunk.css"
+  },
+  {
+    "revision": "1bd9203c75e4bcf9d21c",
+    "url": "/static/js/main.de8c62f3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cdb626d47df3590c655d",
+    "url": "/static/js/2.0e893aa6.chunk.js"
+  },
+  {
+    "revision": "3644ee708bf9efe747ea78b2965ca58f",
+    "url": "/static/media/bg.3644ee70.jpg"
+  },
+  {
+    "revision": "a46853bec327734835d44475a127b4c8",
+    "url": "/index.html"
+  }
+];
